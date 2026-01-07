cfb8a3528a122e9f1db18245e2c0bec24256891f
Jacob L. Walker <jacob.walker@ramseysolutions.com>
2026-01-07 16:55:17 -0600

    add initial happy path integration tests

--- dcat/test/dcat_test.dart
+++ dcat/test/dcat_test.dart
@@ -1,8 +1,44 @@
-import 'package:dcat/dcat.dart';
+import 'dart:io';
+
 import 'package:test/test.dart';
 
 void main() {
-  test('calculate', () {
-    expect(calculate(), 42);
+  group('dcat CLI', () {
+    test('prints numbered lines when -n', () async {
+      final result = await Process.run('dart', [
+        'run',
+        'bin/dcat.dart',
+        'test/fixtures/example.txt',
+        '-n',
+      ]);
+
+      expect(result.exitCode, equals(0));
+      expect(
+        result.stdout,
+        equals('''
+1 Hello World!
+2 This is line two.
+3 Third line here.
+'''),
+      );
+    });
+
+    test('prints lines when -n not passed', () async {
+      final result = await Process.run('dart', [
+        'run',
+        'bin/dcat.dart',
+        'test/fixtures/example.txt',
+      ]);
+
+      expect(result.exitCode, equals(0));
+      expect(
+        result.stdout,
+        equals('''
+Hello World!
+This is line two.
+Third line here.
+'''),
+      );
+    });
   });
 }
